40a0b6b80b3674a431890e4d203979178f915ad5
Arshia <guptaarshia1@gmail.com>
2024-11-19 00:37:37 +0530

    updated

---
+++ stepper_code/README.md
@@ -0,0 +1,1 @@
+This code is for testing of stepper motor. It takes input from serial monitor in the format such that first 2 characters denotes the no. of revolutions, third character is w/s depending on direction and the remaining characters determine the speed of the stepper motor 
---
+++ stepper_code/stepper_code.ino
@@ -0,0 +1,56 @@
+int dir_pin = 9;
+int step_pin = 10;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(step_pin, OUTPUT);
+  pinMode(dir_pin, OUTPUT);
+  digitalWrite(dir_pin, LOW);
+
+  Serial.println("Enter data: ");
+}
+
+void loop() {
+
+  if (Serial.available() > 0) {
+  
+// 10w100 (10 - Revolutions, w-direction, 100 - speed)
+  String input = Serial.readString();
+
+  int speed = input.substring(3).toInt();
+  char direction = input[2];
+  int rev = input.substring(0,2).toInt();
+
+  Serial.println("Speed: ");
+  Serial.println(speed);
+
+  Serial.println("No. of revolutions: ");
+  Serial.println(rev);
+
+  if (direction == 'w'){
+    Serial.println("Direction: Forward");
+  }else if (direction == 's'){
+    Serial.println("Direction: Backward");
+  }
+
+  for (int i = 0; i < 200*rev; i++){
+    if (direction == 'w'){
+      digitalWrite(dir_pin, HIGH); //forward dir
+      digitalWrite(step_pin, HIGH);
+      delay(speed);
+      digitalWrite(step_pin, LOW);
+      delay(speed);
+    } else if (direction == 's'){
+      digitalWrite(dir_pin, LOW); //backward dir
+      digitalWrite(step_pin, HIGH);
+      delay(speed);
+      digitalWrite(step_pin, LOW);
+      delay(speed);
+    } else{
+      Serial.println("Invalid Input");
+    }
+
+  }
+  }
+
